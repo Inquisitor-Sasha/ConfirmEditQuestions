edbf7e6bb21e62653d06a7a37d46585f5cc15130
Inquisitor-sasha <inquisitorsasha@sturmkrieg.de>
2013-08-13 18:58:19 -0400

    Adding the file to the repository

---
+++ ConfirmEditQuestions.php
@@ -0,0 +1,28 @@
+<?php
+
+$wgExtensionCredits[$type][] = array(
+        'path' => __FILE__,
+        'name' => "Rational Wiki pattern Confirm Edit questions",
+        'description' => "Adds a series of questions to ConfirmEdit for preventing spam.",
+        'version' => 1.1,
+        'author' => "Inquisitor Ehrenstein",
+        'url' => "http://www.mediawiki.org/wiki/User:Inquisitor_Ehrenstein",
+);
+
+require_once( "$IP/extensions/ConfirmEdit/QuestyCaptcha.php");
+$wgCaptchaClass = 'QuestyCaptcha';
+$arr = array (
+	"What is the Jewish name of God? (lowercase)" => "yahweh",
+	"What is the Christian name of God? (lowercase)" => "god",
+	"What is the Muslim name of God? (lowercase)" => "allah",
+	"What is the name for false science? (lowercase)" => "pseudoscience",
+	"What is the name for a belief in god(s)? (lowercase)" => "religion",
+	"Who discovered the theory of reletivity? (lowercase, last name)" => "einstein",
+	"Who discovered the theory of evolution? (lowercase, last name) => "darwin",
+	"Who discovered theories about the movements of large objects? (lowercase)" => "newton",
+	"What part of the body is in the logo? (lowercase, one word)" => "brain",
+	"What is the opposite of the first word in the name of the wiki? (lowercase)" => "irrational"
+);
+foreach ( $arr as $key => $value ) {
+        $wgCaptchaQuestions[] = array( 'question' => $key, 'answer' => $value );
+}
